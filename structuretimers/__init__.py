9c9dbf20fdc0fbc324ee8a61b47c527c21fa2947
ErikKalkoken <erik.kalkoken@gmail.com>
2026-07-17 16:56:45 +0200

    chore: bump version for release

--- structuretimers/__init__.py
+++ structuretimers/__init__.py
@@ -5,5 +5,5 @@ with Alliance Auth and Discord.
 # pylint: disable = invalid-name
 default_app_config = "structuretimers.apps.StructureTimersConfig"
 
-__version__ = "3.0.1"
+__version__ = "3.0.2"
 __title__ = "Structure Timers"
